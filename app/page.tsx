3d256f0ec88318cc81a6031e5000f27091f1a4a2
kkarinzzz <notzshop26@gmail.com>
2025-06-18 12:21:14 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -202,7 +202,7 @@ export default function Component() {
                   </div>
                   <div className="flex items-center gap-1">
                     <Users className="h-3 w-3 md:h-4 md:w-4" />
-                    47/100 Pemain
+                    Unlimited Pemain
                   </div>
                 </div>
               </CardContent>
